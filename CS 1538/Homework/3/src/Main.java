f2bcad8f68dbc1f8063b2f914099472c2e2f5dfa
Mark Shanoudy <mathis402@gmail.com>
2014-11-20 06:16:32 -0500

    Finished Accept/Reject and Polar-Coordinate Methods
    - Still needs debugged
    - Main class needs done

--- CS 1538/Homework/3/src/Main.java
+++ CS 1538/Homework/3/src/Main.java
@@ -3,6 +3,23 @@ public class Main
 {
     public static void main(String args[])
     {
+        /* For Testing */
+        VariateGenerator variateGenerator = new VariateGenerator();
 
+        System.out.println("Inverse Transform");
+        variateGenerator.setFilename("InverseTransform.txt");
+        variateGenerator.inverseTransformMethod();
+
+        System.out.println("Accept/Reject - Standard");
+        variateGenerator.setFilename("AcceptReject1.txt");
+        variateGenerator.acceptRejectMethod();
+
+        System.out.println("Accept/Reject - Optimized");
+        variateGenerator.setFilename("AcceptReject2.txt");
+        variateGenerator.acceptRejectMethod2();
+
+        System.out.println("Polar-Coordinate");
+        variateGenerator.setFilename("PolarCoordinate.txt");
+        variateGenerator.polarCoordinateMethod();
     }
 }
--- CS 1538/Homework/3/src/VariateGenerator.java
+++ CS 1538/Homework/3/src/VariateGenerator.java
@@ -5,8 +5,11 @@ import java.util.Random;
 public class VariateGenerator
 {
     private int listSize;
+    private long startTime;
+    private long endTime;
     private String filename;
     private Random random;
+    private ArrayList<Double> variateList;
 
     public VariateGenerator()
     {
@@ -16,36 +19,147 @@ public class VariateGenerator
     public VariateGenerator(int listSize, String filename)
     {
         random = new Random();
+        variateList = new ArrayList<>();
         this.listSize = listSize;
         this.filename = filename;
     }
 
     /**
-     * Performs the Inverse Transform Method on a random number list
+     * Performs the Inverse Transform Method on a set of random numbers
      * Standard Normal CDF: F(x) = 1 / 1 + e^-1.702x
      * Inverse Function: F^-1(x) = -(ln((1/x) - 1) / 1.702)
      */
     public void inverseTransformMethod()
     {
-        ArrayList<Double> variateList = new ArrayList<>();
+        variateList.clear();
 
+        startTimer();
         for (int x = 0; x < listSize; x++)
             variateList.add( -1 * ((Math.log((1 / random.nextDouble()) - 1 )) / 1.702 ));  // Apply inverse function
+        endTimer();
 
-        saveNumbers(variateList);
+        printTimeElapsed();
+        saveNumbers();
     }
 
+    /**
+     * Performs the Accept/Reject Method on a set of random numbers
+     * Exponential Distribution CDF: F(x; λ) 1 - exp(-λx), where λ = 1
+     */
     public void acceptRejectMethod()
     {
-        ArrayList<Double> variateList = new ArrayList<>();
+        variateList.clear();
+
+        double X, U;
+        int successes = 0, failures = 0;
+
+        startTimer();
+        while (successes < listSize)
+        {
+            X = -Math.log(1 - random.nextDouble());         // Inverse transform of g(x): log(1 - u) / (−λ)
+            U = random.nextDouble();                        // U[0,1)
+
+            if (U <= Math.exp(-Math.pow((X - 1), 2) / 2))   // U <= exp(-(X - 1)^2 / 2)
+            {
+                successes++;
+                variateList.add(X);
+            }
+            else
+                failures++;
+        }
+        endTimer();
+
+        printTimeElapsed();
+        System.out.println("Failures: " + failures);
+        saveNumbers();
     }
 
+    /**
+     * Same as acceptRejectMethod() except main test is implemented differently
+     */
+    public void acceptRejectMethod2()
+    {
+        variateList.clear();
+
+        double X1, X2;
+        int successes = 0, failures = 0;
+
+        startTimer();
+        while (successes < listSize)
+        {
+            X1 = -Math.log(1 - random.nextDouble());
+            X2 = -Math.log(1 - random.nextDouble());
+
+            if (X2 >= Math.pow((X1 - 1), 2) / 2) // X2 >= (X1 - 1)^2 / 2
+            {
+                successes++;
+                variateList.add(X1);
+            }
+            else
+                failures++;
+        }
+        endTimer();
+
+        printTimeElapsed();
+        System.out.println("Failures: " + failures);
+        saveNumbers();
+    }
+
+    /**
+     * Performs the Polar-Coordinate Method
+     * Specifically, the Box-Muller Transform
+     */
     public void polarCoordinateMethod()
     {
+        variateList.clear();
 
+        double V1, V2, U, Z1, Z2;
+        int successes = 0;
+
+        startTimer();
+        while (successes < listSize)
+        {
+            V1 = nextDouble(-1, 1);                             // U[-1,1)
+            V2 = nextDouble(-1, 1);                             // U[-1,1)
+            U  = Math.pow(V1, 2) + Math.pow(V2, 2);             // U = V1^2 + V2^2
+
+            if (U < 1)
+            {
+                successes += 2;
+                Z1 = V1 * Math.sqrt((-2 * Math.log(U)) / U);    // Z1 = V1(-2 ln(U) / U)^1/2
+                Z2 = V2 * Math.sqrt((-2 * Math.log(U)) / U);    // Z2 = V2(-2 ln(U) / U)^1/2
+                variateList.add(Z1);
+                variateList.add(Z2);
+            }
+        }
+        endTimer();
+
+        printTimeElapsed();
+        saveNumbers();
     }
 
-    private void saveNumbers(ArrayList<Double> variateList)
+    public void setFilename(String filename)
+    {
+        this.filename = filename;
+    }
+
+    /**
+     * This method is implemented to be equivalent to doubles(Long.MAX_VALUE, randomNumberOrigin, randomNumberBound)
+     * Taken from https://docs.oracle.com/javase/8/docs/api/java/util/Random.html#doubles-double-double-
+     * @param origin The origin (inclusive) of each random value
+     * @param bound The bound (exclusive) of each random value
+     * @return A pseudorandom double value
+     */
+    private double nextDouble(double origin, double bound)
+    {
+        double r = random.nextDouble();
+        r = r * (bound - origin) + origin;
+        if (r >= bound) // Correct for rounding
+            r = Math.nextDown(bound);
+        return r;
+    }
+
+    private void saveNumbers()
     {
         try
         {
@@ -58,5 +172,21 @@ public class VariateGenerator
         {
             System.out.println("Error with writing random number stream to file");
         }
+    }
+
+    private void startTimer()
+    {
+        startTime = System.nanoTime();
+    }
+
+    private void endTimer()
+    {
+        endTime = System.nanoTime();
+    }
+
+    private void printTimeElapsed()
+    {
+        long elapsedTime = (endTime - startTime);
+        System.out.println("Elapsed Time: " + elapsedTime);
     }
 }
