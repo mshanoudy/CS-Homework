6e719a0780967624d119295e3de2974db0f3c400
Mark Shanoudy <mathis402@gmail.com>
2014-11-24 21:08:59 -0500

    Preliminary code reformatting of skeleton

--- CS 1550/Project 4 - File System/cs1550.c
+++ CS 1550/Project 4 - File System/cs1550.c
@@ -15,44 +15,40 @@
 #include <errno.h>
 #include <fcntl.h>
 
-//size of a disk block
-#define	BLOCK_SIZE 512
+#define	BLOCK_SIZE 512  // Size of a disk block
+#define	MAX_FILENAME 8  // Size of filename
+#define	MAX_EXTENSION 3 // Size of file extension
 
-//we'll use 8.3 filenames
-#define	MAX_FILENAME 8
-#define	MAX_EXTENSION 3
-
-//How many files can there be in one directory?
+// How many files can there be in one directory?
 #define	MAX_FILES_IN_DIR (BLOCK_SIZE - (MAX_FILENAME + 1) - sizeof(int)) / \
 	((MAX_FILENAME + 1) + (MAX_EXTENSION + 1) + sizeof(size_t) + sizeof(long))
 
-//How much data can one block hold?
+// How much data can one block hold?
 #define	MAX_DATA_IN_BLOCK BLOCK_SIZE
 
-//How many pointers in an inode?
+// How many pointers in an inode?
 #define NUM_POINTERS_IN_INODE ((BLOCK_SIZE - sizeof(unsigned int) - sizeof(unsigned long)) / sizeof(unsigned long))
 
 struct cs1550_directory_entry
 {
-	char dname[MAX_FILENAME	+ 1];	//the directory name (plus space for a nul)
-	int nFiles;			//How many files are in this directory. 
-					//Needs to be less than MAX_FILES_IN_DIR
+	char dname[MAX_FILENAME	+ 1];	// The directory name (plus space for a nul)
+	int nFiles;			            // How many files are in this directory.
+                                    // Needs to be less than MAX_FILES_IN_DIR
 
 	struct cs1550_file_directory
 	{
-		char fname[MAX_FILENAME + 1];	//filename (plus space for nul)
-		char fext[MAX_EXTENSION + 1];	//extension (plus space for nul)
-		size_t fsize;			//file size
-		long nStartBlock;		//where the first block is on disk
-	} files[MAX_FILES_IN_DIR];		//There is an array of these
+		char fname[MAX_FILENAME + 1];	// Filename (plus space for nul)
+		char fext[MAX_EXTENSION + 1];	// Extension (plus space for nul)
+		size_t fsize;			        // File size
+		long nStartBlock;               // Where the first block is on disk
+	} files[MAX_FILES_IN_DIR];          // There is an array of these
 };
 
 typedef struct cs1550_directory_entry cs1550_directory_entry;
 
 struct cs1550_disk_block
 {
-	//And all of the space in the block can be used for actual data
-	//storage.
+	// And all of the space in the block can be used for actual data storage
 	char data[MAX_DATA_IN_BLOCK];
 };
 
@@ -70,30 +66,33 @@ static int cs1550_getattr(const char *path, struct stat *stbuf)
 
 	memset(stbuf, 0, sizeof(struct stat));
    
-	//is path the root dir?
-	if (strcmp(path, "/") == 0) {
+	// Is path the root dir?
+	if (strcmp(path, "/") == 0)
+    {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else {
+	}
+    else
+    {
 
-	//Check if name is subdirectory
+	// Check if name is subdirectory
 	/* 
-		//Might want to return a structure with these fields
+		// Might want to return a structure with these fields
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 		res = 0; //no error
 	*/
 
-	//Check if name is a regular file
+	// Check if name is a regular file
 	/*
-		//regular file, probably want to be read and write
+		// Regular file, probably want to be read and write
 		stbuf->st_mode = S_IFREG | 0666; 
 		stbuf->st_nlink = 1; //file links
 		stbuf->st_size = 0; //file size - make sure you replace with real size!
 		res = 0; // no error
 	*/
 
-		//Else return that path doesn't exist
+		// Else return that path doesn't exist
 		res = -ENOENT;
 	}
 	return res;
@@ -106,24 +105,24 @@ static int cs1550_getattr(const char *path, struct stat *stbuf)
 static int cs1550_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
-	//Since we're building with -Wall (all warnings reported) we need
-	//to "use" every parameter, so let's just cast them to void to
-	//satisfy the compiler
+	// Since we're building with -Wall (all warnings reported) we need
+	// to "use" every parameter, so let's just cast them to void to
+	// satisfy the compiler
 	(void) offset;
 	(void) fi;
 
-	//This line assumes we have no subdirectories, need to change
+	// This line assumes we have no subdirectories, need to change
 	if (strcmp(path, "/") != 0)
 	return -ENOENT;
 
-	//the filler function allows us to add entries to the listing
-	//read the fuse.h file for a description (in the ../include dir)
+	// The filler function allows us to add entries to the listing
+	// Read the fuse.h file for a description (in the ../include dir)
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
 	/*
-	//add the user stuff (subdirs or files)
-	//the +1 skips the leading '/' on the filenames
+	// Add the user stuff (subdirs or files)
+	// The +1 skips the leading '/' on the filenames
 	filler(buf, newpath + 1, NULL, 0);
 	*/
 	return 0;
@@ -184,11 +183,11 @@ static int cs1550_read(const char *path, char *buf, size_t size, off_t offset,
 	(void) fi;
 	(void) path;
 
-	//check to make sure path exists
-	//check that size is > 0
-	//check that offset is <= to the file size
-	//read in data
-	//set size and return, or error
+	// Check to make sure path exists
+	// Check that size is > 0
+	// Check that offset is <= to the file size
+	// Read in data
+	// Set size and return, or error
 
 	size = 0;
 
@@ -207,11 +206,11 @@ static int cs1550_write(const char *path, const char *buf, size_t size,
 	(void) fi;
 	(void) path;
 
-	//check to make sure path exists
-	//check that size is > 0
-	//check that offset is <= to the file size
-	//write data
-	//set size (should be same as input) and return, or error
+	// Check to make sure path exists
+	// Check that size is > 0
+	// Check that offset is <= to the file size
+	// Write data
+	// Set size (should be same as input) and return, or error
 
 	return size;
 }
